7e779a5fff2c8fbf3fad6ed251875f6aa32eb5de
Jashany <jsingh12_be22@thapar.edu>
2024-12-12 02:36:35 +0530

    notebooks routes updated

--- NodejsBackend/routes/notebook.routes.js
+++ NodejsBackend/routes/notebook.routes.js
@@ -6,17 +6,46 @@ import {
   deleteSegment,
   getNotebook,
   updateSegment,
+  getUserNotebooks
 } from "../controllers/notebook.js";
 import verifyUser from "../middlewares/verifyUser.js";
+import Notebook from "../models/notebook.model.js";
 
 const noteBookRouter = express.Router();
 
 noteBookRouter.post("/create-notebook",verifyUser, createNotebook);
+noteBookRouter.get("/get-chats",verifyUser, getUserNotebooks);
 noteBookRouter.get("/:id",verifyUser, getNotebook);
+
 noteBookRouter.delete("/:id",verifyUser, deleteNotebook);
 noteBookRouter.post("/:id/add-segment",verifyUser, addSegment);
 noteBookRouter.post("/:id/update-segment/:segmentId", updateSegment);
 noteBookRouter.delete("/:id/delete-segment/:segmentId", verifyUser,deleteSegment);
+noteBookRouter.put("/:id/update-title",verifyUser, async (req, res) => {
+  try {
+    const { title } = req.body;
+    const { id } = req.params;
+    const notebook = await Notebook.findOne({
+      notebookId: id,
+    });
+    if (!notebook) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Notebook not found" });
+    }
+    notebook.title = title;
+    await notebook.save();
+    return res.status(200).json({
+      success: true,
+      message: "Notebook title updated",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+});
 
 
-export default noteBookRouter;
+export default noteBookRouter;  
